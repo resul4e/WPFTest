e738dbafee6eef266ddaad464472c5df8778dd4a
resul4e <resul_celik@hotmail.com>
2018-07-18 20:50:17 +0100

    Removed wrong project, replaced with correct project

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharpSvn;
+using System.IO;
+
+namespace TestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : NavigationWindow
+	{
+		public MainWindow()
+		{
+
+			using (SvnClient client = new SvnClient())
+			{
+
+				SvnUriTarget target = new SvnUriTarget(System.IO.Path.GetFullPath("D:/school/programmen/WPFTest/SVN Server"));
+
+				if (client.CheckOut(target, "CodeTest", ))
+				{
+					MessageBox.Show("success");
+				}
+				else
+				{
+					MessageBox.Show("Fail");
+				}
+			}
+
+				InitializeComponent();
+		}
+	}
+}
